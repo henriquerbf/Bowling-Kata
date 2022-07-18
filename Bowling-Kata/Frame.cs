1d9ce28485ef949d9de2b427fcfa22b3173472e1
Henrique Faria <henriquerbf@hotmail.com>
2022-07-17 22:01:52 -0300

    Create Frame.cs

---
+++ Bowling-Kata/Frame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling_Kata
+{
+    public class Frame
+    {
+        private int[] RollPinsDow;
+
+        public Frame(int qtyRolls)
+        {
+            RollPinsDow = new int[qtyRolls];
+        } 
+    }
+}
